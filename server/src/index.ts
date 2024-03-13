d9b5624f1ec60b77b95eb076eafd3aa5dda0f1fb
Jack Hu <jackhu242@gmail.com>
2024-03-13 00:01:31 -0400

    get started with express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
